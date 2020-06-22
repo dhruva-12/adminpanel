76dd43c0f37c2896d9be3b5b758c5e49f0d3af49
Dhruva shah <dhruvashah1221999@gmail.com>
2020-06-22 19:02:35 +0530

    Updates

---
+++ precache-manifest.18cb47960a976c46c1119c009ca6cf04.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1edddc1112f0c70cd2d2ec8559f7af8",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "8dd77b9ec5ea5a44fb20",
+    "url": "/admin/static/css/main.3718956d.chunk.css"
+  },
+  {
+    "revision": "8dcda037ce18b6d5b5f2",
+    "url": "/admin/static/js/2.1f78b544.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/admin/static/js/2.1f78b544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd77b9ec5ea5a44fb20",
+    "url": "/admin/static/js/main.59ce372c.chunk.js"
+  },
+  {
+    "revision": "c57cd1065a063bfe0939",
+    "url": "/admin/static/js/runtime-main.8e0d5b56.js"
+  },
+  {
+    "revision": "93074c953d7b2f4de694e42d47514026",
+    "url": "/admin/static/media/1.93074c95.jpg"
+  },
+  {
+    "revision": "3184c87e90278cfb68a1334770e26ff4",
+    "url": "/admin/static/media/2.3184c87e.jpg"
+  },
+  {
+    "revision": "714cc79740b06c00c88d4c8ac59da288",
+    "url": "/admin/static/media/3.714cc797.jpg"
+  },
+  {
+    "revision": "1267c5ff3f05432df92ab4a9bffb41fa",
+    "url": "/admin/static/media/4.1267c5ff.jpg"
+  },
+  {
+    "revision": "1aa8ac605a5f9aed91950f15b8801236",
+    "url": "/admin/static/media/bg.1aa8ac60.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/admin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/admin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/admin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/admin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/admin/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c77332f603c38a787040215d965ac5c4",
+    "url": "/admin/static/media/micon.c77332f6.woff2"
+  }
+]);
